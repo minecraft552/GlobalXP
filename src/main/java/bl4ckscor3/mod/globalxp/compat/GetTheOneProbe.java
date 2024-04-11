d360bb5d9e4e141d8035ef91f5b3a32cbf575bb5
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-04-11 17:17:04 +0200

    add support for wthit

--- src/main/java/bl4ckscor3/mod/globalxp/compat/GetTheOneProbe.java
+++ src/main/java/bl4ckscor3/mod/globalxp/compat/GetTheOneProbe.java
@@ -3,19 +3,17 @@ package bl4ckscor3.mod.globalxp.compat;
 import java.util.function.Function;
 
 import bl4ckscor3.mod.globalxp.GlobalXP;
-import bl4ckscor3.mod.globalxp.xpblock.XPBlockEntity;
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.IProbeInfoProvider;
 import mcjty.theoneprobe.api.ITheOneProbe;
 import mcjty.theoneprobe.api.ProbeMode;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class GetTheOneProbe implements Function<ITheOneProbe, Void>, IProbeInfoProvider {
+public final class GetTheOneProbe extends HUDOverlayModHandler implements Function<ITheOneProbe, Void>, IProbeInfoProvider {
 	@Override
 	public Void apply(ITheOneProbe theOneProbe) {
 		theOneProbe.registerProvider(this);
@@ -29,11 +27,6 @@ public class GetTheOneProbe implements Function<ITheOneProbe, Void>, IProbeInfoP
 
 	@Override
 	public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, Player player, Level level, BlockState state, IProbeHitData data) {
-		if (level.getBlockEntity(data.getPos()) instanceof XPBlockEntity xpBlock) {
-			probeInfo.horizontal().text(Component.translatable("info.globalxp.levels", String.format("%.2f", xpBlock.getStoredLevels())));
-
-			if (mode == ProbeMode.EXTENDED)
-				probeInfo.horizontal().text(Component.translatable("info.globalxp.xp", xpBlock.getStoredXP()));
-		}
+		addXPInfo(level.getBlockEntity(data.getPos()), mode == ProbeMode.EXTENDED, component -> probeInfo.horizontal().text(component));
 	}
 }
